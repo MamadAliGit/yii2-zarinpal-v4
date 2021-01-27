131997c78f3ae0ff72686d86c35432b22cea99c0
MamadAli <mhmd.ali.shabani@gmail.com>
2021-01-27 11:23:33 +0330

    first commit

--- Zarinpal.php
+++ Zarinpal.php
@@ -5,7 +5,7 @@ namespace mamadali\zarinpal;
 /**
  * This is just an example.
  */
-class AutoloadExample extends \yii\base\Widget
+class Zarinpal extends \yii\base\Widget
 {
     public function run()
     {
